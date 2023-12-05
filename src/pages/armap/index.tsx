af98989b3fec3d1b1d9591985f09522e21cfa803
s1f10210386 <s1f102103864@iniad.org>
2023-12-05 14:18:17 +0900

    ARMAPページ追加

---
+++ src/pages/armap/index.tsx
@@ -0,0 +1,5 @@
+const armap = () => {
+  return <div>Hello World!</div>;
+};
+
+export default armap;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { useRouter } from 'next/router';
+
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container}>Hello World!</div>;
+  const router = useRouter();
+
+  const handleChange = async () => {
+    await router.push('/armap');
+  };
+  return (
+    <div className={styles.container}>
+      <button onClick={handleChange}>ARMAP</button>
+    </div>
+  );
 };
 
 export default Home;
